99f59ac519171511e2a588c41eeeffd45bf52520
Huy Nguyen <hittle9x@gmail.com>
2020-10-16 21:56:45 +0900

    Update run-checkpatch2.sh

--- task1/run-checkpatch2.sh
+++ task1/run-checkpatch2.sh
@@ -3,16 +3,24 @@
 input="./checkpatch-task1/no-merge.txt"
 output="./checkpatch-task1/checkpatch-report-witherror"
 
-rm -rf $output
+#rm -rf $output
 
 declare -i count=1
 declare -i total=`wc -l $input | awk -F " " '{print $1}'`
 
+awk 'NR > 0' $input |
 while read -r line
 do
 	files=`git diff-tree --no-commit-id --name-only -r $line`
-	./scripts/checkpatch.pl --show-types $files 1>> $output 2>> $output
-	((count++))
+# check a commit that has any changed files or not
+	if [ ! -z "$files" ]
+	then
+		./scripts/checkpatch.pl --show-types $files 1>> $output 2>> $output
+	else
+		echo -e "$line is empty\n" 1>> $output 2>> $output 
+	fi
+	
 	unset files
 	echo -e "$count/$total"
-done < "$input"
+	((count++))
+done
